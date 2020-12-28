65e2e0cb426c83d81646b440ac5d96c50d2f8dc2
Ta-wei Yen <yen.david.tawei@gmail.com>
2020-12-29 00:00:01 +0800

    Revert "support dev-dependencies"
    This reverts commit 0e5436a0

--- src/lib.rs
+++ src/lib.rs
@@ -360,3 +360,9 @@ impl<T: ?Sized> Deref for MaybeScoped<'_, T> {
 pub fn build_script() {
     // Do nothing. just forcing env var OUT_DIR to be set.
 }
+
+#[test]
+fn compile_test() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/*.rs");
+}
