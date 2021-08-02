ec390c12d50cdcd7f3c071da51f7261b6e3ce291
azureblaze <40325793+azureblaze@users.noreply.github.com>
2021-08-02 15:59:29 +0800

    #[multibinds] (#95)

---
+++ compile_tests/tests/module/module_multibinds_has_args.rs
@@ -0,0 +1,31 @@
+/*
+Copyright 2021 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+extern crate lockjaw;
+
+use lockjaw::{builder_modules, component, injectable, module, qualifier, subcomponent, Cl};
+
+lockjaw::prologue!(
+    "../../../compile_tests/tests/module/module_multibinds_has_args.rs",
+    ""
+);
+pub struct S {}
+
+#[module]
+impl S {
+    #[multibinds]
+    pub fn vec(&self) -> Vec<String> {}
+}
+lockjaw::epilogue!(test);
